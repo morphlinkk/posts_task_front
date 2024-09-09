77049e37b7f7618868acd079af7601d898b54709
morphlinkk <antonvolkov2803@gmail.com>
2024-09-09 18:11:25 +0300

    added store caching

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
     <div class="max-w-[1280px] mx-auto">
         <div class="w-full flex justify-between items-center mb-3 mt-1 pl-3"></div>
         <div class="">
-        <h3 class="text-lg font-semibold text-slate-800 ml-4">Posts</h3>
-        <div class="flex items-center justify-between px-4 py-3">
-            <p class="text-slate-500">Table with posts</p>
-            <button @click="openModal" class="font-bold text-slate-700">Create Post</button>
-        </div>
+            <h3 class="text-lg font-semibold text-slate-800 ml-4">Posts</h3>
+            <div class="flex items-center justify-between px-4 py-3">
+                <p class="text-slate-500">Table with posts</p>
+                <button @click="openModal" class="font-bold text-slate-700">Create Post</button>
+            </div>
         </div>
         <div class="relative flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
             <table class="w-full text-left table-fixed min-w-max">
@@ -33,21 +33,21 @@
                         </tr>
                     </template>
                     <template v-else>
-                        <tr v-for="post in posts" :key="post.id" class="hover:bg-slate-50 border-b border-slate-200 h-16">
+                        <tr v-for="post in store.currentPagePosts" :key="post.id" class="hover:bg-slate-50 border-b border-slate-200 h-16">
                             <PostTemplate :post="post" />
                         </tr>
                     </template>
                 </tbody>
             </table>
-            <div class="flex justify-between items-center px-4 py-3" v-if="posts.length!=0">
+            <div class="flex justify-between items-center px-4 py-3" v-if="store.totalPostsCount">
                 <div class="text-sm text-slate-500">
-                Showing <b>{{ displayStartIndex  }}-{{ displayEndIndex }}</b> of {{ store.totalPostsCount }}
+                    Showing <b>{{ displayStartIndex }} - {{ displayEndIndex }}</b> of {{ store.totalPostsCount }}
                 </div>
                 <div class="flex space-x-1">
                     <button @click="prevPage" class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                         Prev
                     </button>
-                    <button @click="() => { store.page = page; store.getPostsList() }" v-for="page in store.totalPages" :key="page" class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
+                    <button @click="() => goToPage(page)" v-for="page in store.totalPages" :key="page" class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
                         {{ page }}
                     </button>
                     <button @click="nextPage" class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
@@ -62,12 +62,24 @@
 
 <script setup lang="ts">
 import { usePostStore } from '~/store/posts';
-import { storeToRefs } from 'pinia';
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 const store = usePostStore();
-store.getPostsList();
-const { posts } = storeToRefs(store);
+
+const showModal = ref(false);
+
+function openModal() {
+    showModal.value = true;
+}
+
+function closeModal() {
+    showModal.value = false;
+}
+
+function goToPage(page: number) {
+    store.page = page;
+    store.getPostsList();
+}
 
 function prevPage() {
     if (store.page > 1) {
@@ -83,29 +95,16 @@ function nextPage() {
     }
 }
 
-const showModal = ref(false);
-
-function openModal() {
-    showModal.value = true;
-}
-
-function closeModal() {
-    showModal.value = false;
-}
-
 const displayStartIndex = computed(() => {
-    if (store.sortOrder === 'asc') {
-        return (store.page - 1) * store.limit + 1;
-    } else {
-        return store.totalPostsCount - (store.page * store.limit - store.limit);
-    }
+    return ((store.page-1)*store.limit)+1;
 });
 
 const displayEndIndex = computed(() => {
-    if (store.sortOrder === 'asc') {
-        return Math.min(store.page * store.limit, store.totalPostsCount);
-    } else {
-        return store.totalPostsCount - (store.page  * store.limit) + 1;
-    }
+    const calculatedIndex = (store.page*store.limit)
+    return (calculatedIndex>store.totalPostsCount) ? store.totalPostsCount : calculatedIndex
 });
+
+watch(() => store.page, () => {
+    store.getPostsList();
+}, { immediate: true });
 </script>
--- store/posts.ts
+++ store/posts.ts
@@ -1,5 +1,3 @@
-import { defineStore } from 'pinia';
-
 export const usePostStore = defineStore('posts', {
     state: () => ({
         posts: [] as { id: number; userId: number; title: string; body: string}[],
@@ -7,11 +5,19 @@ export const usePostStore = defineStore('posts', {
         showThrobber: true,
         page: 1,
         limit: 10,
-        sortOrder: 'asc'
+        sortOrder: 'asc',
     }),
     getters: {
         totalPages(state) {
             return Math.ceil(state.totalPostsCount / state.limit);
+        },
+        currentPagePosts(state) {
+            const start = ((state.page-1)*state.limit)+1
+            const end = (state.page*state.limit)
+            const postsArray = Object.values(state.posts);
+
+            const filteredPosts = postsArray.filter(post => post.id >= start && post.id <= end);
+            return filteredPosts
         }
     },
     actions: {
@@ -20,12 +26,17 @@ export const usePostStore = defineStore('posts', {
             try {
                 const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${this.page}&_limit=${this.limit}&_sort=id&_order=${this.sortOrder}`);
                 const data = await response.json();
-                
-                this.totalPostsCount = parseInt(response.headers.get('X-Total-Count') ?? '0');
-                
-                this.posts = data.map((post: { id: number; userId: number; title: string; body: string }) => ({
-                    ...post
-                }));
+                const serverPostsCount = parseInt(response.headers.get('X-Total-Count') ?? '0')
+                if (this.totalPostsCount < serverPostsCount){this.totalPostsCount=serverPostsCount};
+
+                data.forEach((post: { id: number; userId: number; title: string; body: string }) => {
+                    this.posts[post.id] = {
+                        id: post.id,
+                        userId: post.userId,
+                        title: post.title,
+                        body: post.body
+                    };
+                });
             } catch (error) {
                 console.error('Failed to fetch posts:', error);
             } finally {
@@ -35,6 +46,40 @@ export const usePostStore = defineStore('posts', {
         toggleSortOrder() {
             this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
             this.getPostsList();
-        }
+        },
+        async createPost(post: { userId: number; title: string; body: string }) {
+            this.showThrobber = true;
+            let responsePost = {};
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                    method: 'POST',
+                    body: JSON.stringify(post),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    },
+                });
+    
+                if (response.ok) {
+                    const createdPost = await response.json();
+                    
+                    this.posts.push({
+                        id: createdPost.id,
+                        userId: createdPost.userId,
+                        title: createdPost.title,
+                        body: createdPost.body,
+                    });
+                    
+                    this.totalPostsCount += 1;
+                    responsePost = createdPost
+                } else {
+                    console.error('Failed to create post:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Failed to create post:', error);
+            } finally {
+                this.showThrobber = false;
+                return responsePost
+            }
+        },
     },
 });
